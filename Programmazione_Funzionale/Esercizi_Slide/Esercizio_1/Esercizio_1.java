891c98e2f6fca95b047e2a6b7e15d0bf1d74dfe8
PriviteraGabriele <102214867+PriviteraGabriele@users.noreply.github.com>
2023-06-16 17:21:21 +0200

    Prove

---
+++ Programmazione_Funzionale/Esercizi_Slide/Esercizio_1/Esercizio_1.java
@@ -0,0 +1,17 @@
+package Ingegneria_del_Software.Programmazione_Funzionale.Esercizi_Slide.Esercizio_1;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+public class Esercizio_1 {
+    public static void main(String [] args){
+        List<String>  l1 = List.of("author", "auto","aucorrect","begin","big","bigger","biggish");
+
+        final String pref = "aut";
+        List<String> r = l1.stream()
+                           .filter(s -> s.startsWith(pref))
+                           .collect(Collectors.toList());
+
+        r.forEach(System.out::println);
+    }    
+}
